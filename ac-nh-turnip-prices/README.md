26ef76ad229596dffffe89f9b2b5c7f9866be4b0
graiffgarcia <graiffgarcia.1@osu.edu>
2021-02-11 09:24:47 -0500

    adding ac-nh-turnip-prices

---
+++ ac-nh-turnip-prices/README.md
@@ -0,0 +1,7 @@
+# Animal Crossing New Horizons: Turnip Prophet
+
+Turnip Prophet is a price calculator/price predictor for Animal Crossing: New Horizons turnip prices.
+
+The purpose of this fork is to predict how many Bells you will receive upon selling your turnips. It includes an input option wherein you can include the amount of turnips you bought on Sunday. It then gives you the amount of Bells that you would receive if you sell at the guaranteed minimum or potential maximum values, for each row on the table. Everything else in the code is the exact same as the original.
+
+As I'm forking this repo for my personal use only, I have not included any translations of any of the new text fields I created. The new text works in English only.
---
+++ ac-nh-turnip-prices/js/translations.js
@@ -0,0 +1,75 @@
+function updateContent() {
+  update();
+  $('body').localize();
+}
+const defaultLanguage = 'en';
+const LANGUAGES = {
+  'ca': 'Català',
+  'cs': 'Česky',
+  'de': 'Deutsch',
+  'en': 'English',
+  'es': 'Español',
+  'fr': 'Français',
+  'gl': 'Galego',
+  'hu': 'magyar',
+  'id': 'Bahasa Indonesia',
+  'it': 'Italiano',
+  'ja': '日本語',
+  'ko': '한국어',
+  'nl': 'Nederlands',
+  'ph': 'Filipino',
+  'pl': 'Polski',
+  'pt-BR': 'Português',
+  'ru': 'Русский',
+  'ua': 'Українська',
+  'th': 'ไทย',
+  'zh-CN': '简体中文',
+  'zh-TW': '繁體中文'
+};
+i18next
+.use(i18nextXHRBackend)
+.use(i18nextBrowserLanguageDetector)
+.init({
+  fallbackLng: defaultLanguage,
+  debug: true,
+  backend: {
+    loadPath: 'locales/{{lng}}.json',
+  },
+}, (err, t) => {
+  languageSelector = $('#language');
+  for (let [code, name] of Object.entries(LANGUAGES)) {
+    languageSelector.append(`<option value="${code}">${name}</option>`);
+  }
+  for (let code of i18next.languages) {
+    if (code in LANGUAGES) {
+      languageSelector.val(code);
+      $('html').attr('lang', code);
+      break;
+    }
+  }
+  languageSelector.on('change', function () {
+    if (this.value == i18next.language)
+      return;
+    i18next.changeLanguage(this.value);
+    $('html').attr('lang', this.value);
+  });
+  jqueryI18next.init(i18next, $);
+  i18next.on('languageChanged', lng => {
+    updateContent();
+  });
+  // init set content
+  $(document).ready(initialize);
+
+  let delayTimer;
+  $(document).on('input', function(event) {
+    //prevent radio input from updating content twice per input change
+    if(event.target.type === 'radio'){ return }
+    // adding short delay after input to help mitigate potential lag after keystrokes
+    clearTimeout(delayTimer);
+    delayTimer = setTimeout(function() {
+      updateContent();
+    }, 500);
+  });
+
+  $('input[type = radio]').on('change', updateContent);
+});
